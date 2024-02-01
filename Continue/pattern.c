e83ae1d03f3bae0728cacc5e7a47c700273a992b
charmisojitra123 <skillqodecharmi@gmail.com>
2024-02-01 16:05:45 +0530

    pattern

--- Continue/pattern.c
+++ Continue/pattern.c
@@ -13,6 +13,29 @@ int main()
 		printf("%d ",i);
 	}
 	
+	
+
+
+
+
+	int i,j;
+	
+	for(i=1;i<=10;i++){
+		for(j=1;j<=5;j++){
+			if(i%2==!0){
+			printf("%d ",i);
+			}
+			/*else if(i%2==0){
+			printf("");
+			}*/
+			else{
+			printf("");
+			}
+		}
+		
+		printf("\n");
+	}
+	
 	return 0;
 	
 }
